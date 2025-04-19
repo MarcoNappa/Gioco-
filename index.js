649ceff99415353bc0ecc2fb3a217c2c0693eaf1
MarcoNappa <marcomarconappa@gmail.com>
2025-04-20 01:20:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const livelli = [
+    { testo: "DIO CANE" },
+    { testo: "DIO POVERO" },
+    { testo: "MADONNA LADRA" },
+    { testo: "MANNAGGIA CRISTO" },
+    { testo: "DIO BESTIO" },
+    { testo: "PORCA MADONNA" },
+    { testo: "DIO SERPENTE" },
+    { testo: "DIO SOLFITO" },
+    { testo: "SANDALI DI CRISTO" },
+    { testo: "MANNAGGIA LA BIBBIA"},
+  ];
+
+  let livelloCorrente = 0;
+  
+  const suono =document.getElementById("suono");
+
+  const contenitore = document.getElementById("contenitore");
+
+  const cambiaLivello = document.getElementById("bottoneLV");
+
+  const paragrafo = document.createElement("p");
+
+  paragrafo.textContent = "SEI ALL' INIZIO DEL NUOVO BESTEMMIATOR 9000 PLUS";
+
+  contenitore.appendChild(paragrafo);
+
+  cambiaLivello.addEventListener("click", function cambiaLv() {
+    if (livelloCorrente < livelli.length) {
+      const paragrafoSucc = document.createElement("p");
+      paragrafoSucc.textContent = livelli[livelloCorrente].testo;
+      contenitore.appendChild(paragrafoSucc);
+      livelloCorrente++;
+      document.getElementById("suono").play();
+    } else {
+      const fine = document.createElement("p");
+      fine.textContent = "E PORCODDDDDIOOOOOOOOO BASTA MADONNA SCALZA , CAZZO TI BESTEMMI MERDA UMANA... DIO CANE MADONNA !!!!!"
+      contenitore.appendChild(fine);
+      cambiaLivello.disabled = true;
+    };
+  });
+});
+
+
+
+
+
+
+
